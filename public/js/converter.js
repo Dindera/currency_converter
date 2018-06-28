be510cac721a11a4f582a5f49cf6edb65306b243
Chidera Ozoh <chideraozoh@icloud.com>
2018-06-28 17:12:08 +0100

    converter updated

--- public/js/converter.js
+++ public/js/converter.js
@@ -53,6 +53,50 @@ class FreeCurrencyConverter {
     }
   }
 
+  function convertAmount(conversionRate){
+    let resultElement = document.getElementById('conversion');
+    resultElement.innerText = "";
+  
+    let from = document.getElementById('from-currency').value;
+    let to = document.getElementById('to-currency').value;
+  
+    let amount = parseFloat(document.getElementById('amount').value);
+  
+    conversionRate.convert(from, to, amount, (err, amount) => {
+        console.log(amount);
+        console.log(typeof amount);
+  
+  
+        if (amount <= 0 || isNaN(amount)) {
+            amount = '0.00';
+        }
+  
+        resultElement.innerText = `${to} ${amount}`;
+    });
+  }
+  
+  
+  function populateCurrencyOptions(conversionRate){
+  
+    let elements = [
+        document.getElementById('from-currency'),
+        document.getElementById('to-currency')
+    ];
+  
+    conversionRate.getCountries((countryAbbreviations) => {
+        console.log('countryAbbreviations: ', countryAbbreviations);
+  
+        for (let el in elements) {
+            for (let countryAbbreviation in countryAbbreviations) {
+                let option = document.createElement("option");
+                option.text = `${countryAbbreviations[countryAbbreviation]['name']} - ${countryAbbreviations[countryAbbreviation]['currencyId']}`;
+                option.value = countryAbbreviations[countryAbbreviation]['currencyId'];
+                elements[el].add(option);
+            }
+        }
+    })
+  }
+
 
 
 window.addEventListener('DOMContentLoaded', function() {
