737bf0237e9d28b3fe8447bb2d9945c578e0d857
Chidera Ozoh <chideraozoh@icloud.com>
2018-07-01 19:40:13 +0100

    updates

--- sw.js
+++ sw.js
@@ -2,86 +2,89 @@
 const cacheName = "currency_converter_v1";
 const convertCache = "currency_converter_idb";
 const cacheAll = [cacheName, convertCache];
-const  REPO = '/currency_converter';///currency_converter
+const REPO = '/currency_converter';///currency_converter
 
- /*
+/*
 Install caches in storage
 */
 self.addEventListener("install", event => {
-  console.log("Service Worker Installing");
-  event.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll([
-        REPO+"/index.html",
-        REPO+ "/sw.js",
-        REPO+"/public/css/styles.css",
-        REPO+"/public/js/converter.js",
-        REPO+"/public/js/indexedb/idb.js",
-        REPO+"/public/css/favicon.png",
-        "https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css",
-        "https://free.currencyconverterapi.com/api/v5/countries",
-        "https://free.currencyconverterapi.com/api/v5/convert?q=USD_PHP&compact=y"
-      ]);
-    })
-  );
+    console.log("Service Worker Installing");
+    event.waitUntil(
+        caches.open(cacheName).then(cache => {
+            return cache.addAll([
+                REPO + "/index.html",
+                REPO + "/sw.js",
+                REPO + "/public/css/styles.css",
+                REPO + "/public/js/converter.js",
+                REPO + "/public/js/indexedb/idb.js",
+                REPO + "/public/css/favicon.png",
+                "https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css",
+                "https://free.currencyconverterapi.com/api/v5/countries",
+                "https://free.currencyconverterapi.com/api/v5/convert?q=USD_PHP&compact=y"
+            ]);
+        })
+    );
 });
 
 // event for activate when new cache name is discovered
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(cacheFiles => {
-      return Promise.all(
-        cacheFiles
-          .filter(cacheName => {
-            return (
-              cacheName.startsWith("currency_") && !cacheAll.includes(cacheName)
+    console.log('StarACT');
+    self.clients.claim();
+    console.log('AAACLMDONE');
+    event.waitUntil(
+        caches.keys().then(cacheFiles => {
+            return Promise.all(
+                cacheFiles
+                    .filter(cacheName => {
+                        return (
+                            cacheName.startsWith("currency_") && !cacheAll.includes(cacheName)
+                        );
+                    })
+                    .map(cacheName => {
+                        return caches.delete(cacheName);
+                    })
             );
-          })
-          .map(cacheName => {
-            return caches.delete(cacheName);
-          })
-      );
-    })
-  );
+        })
+    );
 });
 
 
 self.addEventListener("fetch", event => {
-  let getUrl = new URL(event.request.url);
+    let getUrl = new URL(event.request.url);
 
-  if (getUrl.origin === location.origin) {
-    console.log("fetching url :" + getUrl.pathname);
-    //handles only index 
-    if (getUrl.pathname === "/" || getUrl === REPO+"/") {
-        console.log('INDXAAAAA',getUrl.pathname );
-      event.respondWith(caches.match(REPO+"/index.html"));
+    if (getUrl.origin === location.origin) {
+        console.log("fetching url :" + getUrl.pathname);
+        //handles only index 
+        if (getUrl.pathname === "/" || getUrl === REPO + "/") {
+            console.log('INDXAAAAA', getUrl.pathname);
+            event.respondWith(caches.match(REPO + "/index.html"));
+        }
+        else {// handle other request from  this site
+            console.log('OTHRSAAAAA', getUrl.pathname);
+            event.respondWith(caches.match(event.request));
+        }
+        return;
     }
-    else{// handle other request from  this site
-        console.log('OTHRSAAAAA',getUrl.pathname );
-       event.respondWith(caches.match(event.request)); 
-    }
-    return;
-  }
 
-  //this only handles external request alone
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      if (response) {
-        return response;
-      } else {
-        console.log(
-          event.request.url + " not found in cache fetching from network."
-        );
-        return fetch(event.request);
-      }
-    })
-  );
+    //this only handles external request alone
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            if (response) {
+                return response;
+            } else {
+                console.log(
+                    event.request.url + " not found in cache fetching from network."
+                );
+                return fetch(event.request);
+            }
+        })
+    );
 });
 
 
 
 self.addEventListener("message", event => {
-  if (event.data.action === "skipWaiting") {
-    self.skipWaiting();
-  }
+    if (event.data.action === "skipWaiting") {
+        self.skipWaiting();
+    }
 });
