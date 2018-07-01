861de53a07b0273586d6e31e1e7947207fd8f189
Chidera Ozoh <chideraozoh@icloud.com>
2018-07-01 22:43:59 +0100

    Latest update

--- sw.js
+++ sw.js
@@ -54,7 +54,7 @@ self.addEventListener("fetch", event => {
     if (getUrl.origin === location.origin) {
         console.log("fetching url :" + getUrl.pathname);
         //handles only index 
-        if (getUrl.pathname === "/" || getUrl === REPO + "/") {
+        if (getUrl.pathname === "/" || getUrl.pathname === REPO + "/") {
             console.log('INDXAAAAA', getUrl.pathname);
             event.respondWith(caches.match(REPO + "/index.html"));
         }
