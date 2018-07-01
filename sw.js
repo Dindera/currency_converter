993182c92011f6161058e088cfc4e0e04edf305e
Chidera Ozoh <chideraozoh@icloud.com>
2018-07-01 23:24:45 +0100

    Last service Worker commit

--- sw.js
+++ sw.js
@@ -1,66 +1,86 @@
+const cacheName = "currency_converter_v1";
+const convertCache = "currency_converter_idb";
+const cacheAll = [cacheName, convertCache];
+const REPO = '/currency_converter';// or const REPO = ''; //for local hosting
 
-let cacheName = "currency_converter_v1";
-let convertCache = 'currency_converter_ids'
-let cacheAll = [cacheName, convertCache];
-
-
+/*
+Install caches in storage
+*/
 self.addEventListener("install", event => {
-    console.log('Service Worker Installing');
+    console.log("Service Worker Installing");
     event.waitUntil(
-            caches.open(cacheName).then((cache) => { //catch that promise
-                            console.log('Service Worker Caching files');
-                           return cache.addAll([
-                               '/skeleton',
-                               './index.html',
-                               './public/css/styles.css',
-                               './public/js/converter.js',
-                               './public/js/sw/registersw.js',
-                               'https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css',
-                               'https://free.currencyconverterapi.com/api/v5/countries',
-                               'https://free.currencyconverterapi.com/api/v5/convert?q=USD_PHP&compact=y',
-                               
-                              
-                           ]);
-                        })
-                    ); 
+        caches.open(cacheName).then(cache => {
+            return cache.addAll([
+                REPO + "/index.html",
+                REPO + "/sw.js",
+                REPO + "/public/css/styles.css",
+                REPO + "/public/js/converter.js",
+                REPO + "/public/js/indexedb/idb.js",
+                REPO + "/public/css/favicon.png",
+                "https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css",
+                "https://free.currencyconverterapi.com/api/v5/countries",
+                "https://free.currencyconverterapi.com/api/v5/convert?q=USD_PHP&compact=y"
+            ]);
+        })
+    );
+});
+
+// event for activate when new cache name is discovered
+self.addEventListener("activate", event => {
+    self.clients.claim();
+    event.waitUntil(
+        caches.keys().then(cacheFiles => {
+            return Promise.all(
+                cacheFiles
+                    .filter(cacheName => {
+                        return (
+                            cacheName.startsWith("currency_") && !cacheAll.includes(cacheName)
+                        );
+                    })
+                    .map(cacheName => {
+                        return caches.delete(cacheName);
+                    })
+            );
+        })
+    );
+});
+
+
+self.addEventListener("fetch", event => {
+    let getUrl = new URL(event.request.url);
+
+    if (getUrl.origin === location.origin) {
+        console.log("fetching url :" + getUrl.pathname);
+        //handles only index 
+        if (getUrl.pathname === "/" || getUrl.pathname === REPO + "/") {
+            console.log('INDXAAAAA', getUrl.pathname);
+            event.respondWith(caches.match(REPO + "/index.html"));
+        }
+        else {// handle other request from  this site
+            console.log('OTHRSAAAAA', getUrl.pathname);
+            event.respondWith(caches.match(event.request));
+        }
+        return;
+    }
+    //this only handles external request alone
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            if (response) {
+                return response;
+            } else {
+                console.log(
+                    event.request.url + " not found in cache fetching from network."
+                );
+                return fetch(event.request);
+            }
+        })
+    );
 });
 
 
 
-self.addEventListener('activate', (event) => {
-    event.waitUntil(
-                  caches.keys().then((cacheFiles) => {
-                return Promise.all(
-                    cacheFiles.filter(cacheName => {
-                        return cacheName.startsWith('currency_') && 
-                        !cacheAll.includes(cacheName);
-                      }).map(cacheName => {
-                        return caches.delete(cacheName);
-                      })
-                );
-             })
-        );
-    })
-
-self.addEventListener("fetch", (event) => {
-    let getUrl = new URL(event.request.url);
-   
-    if(getUrl.origin === location.origin){
-       if(getUrl.pathname === '/'){
-           event.respondWith(caches.match('./index.html'));
-           return;
-       }
+self.addEventListener("message", event => {
+    if (event.data.action === "skipWaiting") {
+        self.skipWaiting();
     }
-       event.respondWith(caches.match(event.request).then(response => {
-                 if(response) {	
-                         console.log(`Found ${getUrl} from cache, refresh to update`);
-                                 return response;
-                                }else {
-                                    console.log(event.request.url+" not found in cache fetching from network.");
-                                    return fetch(event.request);
-                                }
-                            })
-                        );
-           });
-
-
+});
